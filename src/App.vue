e779575eec480c61a38ebd3e176e7680ca2696b1
Chandanindalkar <chandan.s.indalkar@gmail.com>
2024-03-14 12:05:39 +0530

    used template refs to store an element in a variable to perform javascript on it

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>{{ title }}</h1>
+  <h1>{{ desc }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  <h2 v-if="bool">class added to the ref element</h2>
 </template>
 
 <script>
@@ -8,11 +12,21 @@
 export default {
   data() {
     return {
-      title: "Yet Another Vue App"
+      title: "Template Refs",
+      desc: "click on the below input field",
+      bool: false
     }
   },
   name: 'App', // optional
   components: {
+
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.bool = !this.bool
+    },
 
   }
 }
@@ -26,5 +40,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.active {
+  background-color: aquamarine;
+  border-color: blue;
+  color: blue;
+  border-width: 10px;
 }
 </style>
